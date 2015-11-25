f8ffbacb52fdf550b9835e4e8a62f32c661d30ce
EvolveZy <840912357@qq.com>
2015-11-25 22:34:37 +0800

    Create Myclass.cs

---
+++ Myclass.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MYAIR
+{
+    class Class1
+    {
+        public static string ClientVersion = "1.15.9.18";
+        public static string ErrInfo = "802BB804367DC69B3475290FE5A65164";
+        public static string ServerPage = "http://100.0.0.10/NSUAAAWS/Default.asmx";
+        public static string Encrypt(string sourceString, string key, string iv)
+        {
+            try
+            {
+                byte[] btKey = Encoding.UTF8.GetBytes(key);
+
+                byte[] btIV = Encoding.UTF8.GetBytes(iv);
+
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] inData = Encoding.UTF8.GetBytes(sourceString);
+                    try
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(btKey, btIV), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inData, 0, inData.Length);
+
+                            cs.FlushFinalBlock();
+                        }
+
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                    catch
+                    {
+                        return sourceString;
+                    }
+                }
+            }
+            catch { }
+
+            return "DES加密出错";
+        }
+        public static string Decrypt(string encryptedString, string key, string iv)
+        {
+            byte[] btKey = Encoding.UTF8.GetBytes(key);
+
+            byte[] btIV = Encoding.UTF8.GetBytes(iv);
+
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] inData = Convert.FromBase64String(encryptedString);
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(btKey, btIV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inData, 0, inData.Length);
+
+                        cs.FlushFinalBlock();
+                    }
+
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+                catch
+                {
+                    return encryptedString;
+                }
+            }
+        }  
+    }
+}
